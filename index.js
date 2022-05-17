8dc7ccbb99c9ab5af5ebfaadc804d7e0c15f5dbd
Nike Spengler <32640656+NikeSpengler@users.noreply.github.com>
2022-05-17 14:43:50 +0200

    update

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
-
+import authRoute from "./routes/auth.js";
 const app = express();
 dotenv.config();
 
+//connect to bakcend server
 const connect = async () => {
     try {
         await mongoose.connect(process.env.MONGO);
@@ -13,6 +14,13 @@ const connect = async () => {
         throw error;
     }
 };
+
+mongoose.connection.on("disconnected", () => {
+    console.log("mongoDB disconnected!")
+})
+
+//Middleware
+app.use("/api/auth", authRoute);
 
 
 app.listen(8800, () => {
